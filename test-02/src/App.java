07622e1622e2f70bc370d2aa84a216812d7df912
Renan Miquilito <renandgmiquilito@gmail.com>
2024-04-27 20:40:52 -0300

    Add printing of thread execution logs to the console

--- test-02/src/App.java
+++ test-02/src/App.java
@@ -28,5 +28,65 @@ public class App {
         for (int i = 0; i < (2 * 2); i++) {
             employees.get(i).start();
         }
+
+        try {
+            for (Customer customer : customers) {
+                customer.join();
+            }
+        } catch (InterruptedException e) {
+            System.out.println(e);
+        }
+
+        try {
+            for (Employee employee : employees) {
+                employee.join();
+            }
+        } catch (InterruptedException e) {
+            System.out.println(e);
+        }
+
+        for (Store store : stores) {
+            System.out.println("\n\t" + store.getName() + ".account.statement\n");
+            System.out.println(store.getAccount().getStatement());
+        }
+        System.out.println();
+
+        for (Employee employee : employees) {
+            System.out.println("\n\t" + employee.getName() + ".salaryAccount.statement\n");
+            System.out.println(employee.getSalaryAccount().getStatement());
+        }
+        System.out.println();
+
+        for (Employee employee : employees) {
+            System.out.println("\n\t" + employee.getName() + ".investmentAccount.statement\n");
+            System.out.println(employee.getInvestmentAccount().getStatement());
+        }
+        System.out.println();
+
+        for (Customer customer : customers) {
+            System.out.println("\n\t" + customer.getName() + ".account.statement\n");
+            System.out.println(customer.getAccount().getStatement());
+        }
+        System.out.println();
+
+        System.out.println();
+        for (Store store : stores) {
+            System.out.println(store.getName() + "? " + "R$" + store.getAccount().getBalance());
+        }
+
+        System.out.println();
+        for (Employee employee : employees) {
+            System.out.println(employee.getName() + "? " + "R$" + employee.getSalaryAccount().getBalance());
+        }
+
+        System.out.println();
+        for (Employee employee : employees) {
+            System.out.println(employee.getName() + "? " + "R$" + employee.getInvestmentAccount().getBalance());
+        }
+
+        System.out.println();
+        for (Customer customer : customers) {
+            System.out.println(customer.getName() + "? " + "R$" + customer.getAccount().getBalance());
+        }
     }
 }
