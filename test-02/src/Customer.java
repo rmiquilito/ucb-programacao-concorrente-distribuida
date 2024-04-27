c12042f25ce800d644f09b30bea6965441876b71
Renan Miquilito <renandgmiquilito@gmail.com>
2024-04-27 20:26:04 -0300

    refactor: override the run() method to follow customer business rules

--- test-02/src/Customer.java
+++ test-02/src/Customer.java
@@ -17,4 +17,29 @@ public class Customer extends Thread {
     public Account getAccount() {
         return this.account;
     }
+
+    public Double decideCost() {
+        Double value = ((1 + new Random().nextInt(2)) * 100.0);
+        if (this.account.getBalance() == 100.0) {
+            value = 100.0;
+        }
+
+        return value;
+    }
+
+    public Store chooseStore() {
+        Store store = (Store) this.bank.getStoreAccounts().get(this.targetStore).getHolder();
+        this.targetStore = (this.targetStore + 1) % 2;
+
+        return store;
+    }
+
+    public void run() {
+        while (!this.account.scan()) {
+            Store store = chooseStore();
+            Double cost = decideCost();
+
+            this.account.deposit(cost, store.getAccount());
+        }
+    }
 }
