77e98f487d82de38afb1db39d584ab781aaf14f2
Renan Miquilito <renandgmiquilito@gmail.com>
2024-04-27 20:30:24 -0300

    refactor: override the run() method to follow employee business rules

--- test-02/src/Employee.java
+++ test-02/src/Employee.java
@@ -26,4 +26,41 @@ public class Employee extends Thread {
     public Account getInvestmentAccount() {
         return this.investmentAccount;
     }
+
+    public Employee getCoworker() {
+        int i = 0;
+        for (Employee employee : this.store.getEmployees()) {
+            if (employee != this) {
+                i = this.store.getEmployees().indexOf(employee);
+            }
+        }
+
+        return this.store.getEmployees().get(i);
+    }
+
+    public void run() {
+        while (!this.store.communicate() || this.store.funds()) {
+            Boolean lateWithdraw = this.salaryAccount.getStatement().size() < this.getCoworker().getSalaryAccount()
+                    .getStatement().size();
+            Boolean notLateWithdraw = this.salaryAccount.getStatement().size() == this.getCoworker().getSalaryAccount()
+                    .getStatement().size();
+
+            if (lateWithdraw) {
+                if (this.store.getAccount().getBalance() >= 700.0) {
+                    this.salaryAccount.withdraw(this.store.getAccount(), 700.0);
+                    this.salaryAccount.deposit(700.0 * 0.2, this.getInvestmentAccount());
+                }
+
+            }
+
+            if (notLateWithdraw) {
+                if (this.store.getAccount().getBalance() >= 1400.0) {
+                    this.salaryAccount.withdraw(this.store.getAccount(), 700.0);
+                    this.salaryAccount.deposit(700.0 * 0.2, this.getInvestmentAccount());
+                } else if (this.store.communicate()) {
+                    return;
+                }
+            }
+        }
+    }
 }
