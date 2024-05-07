a8e3670e5640f63bd32922c5f1b34b0d9d1cf382
Renan Miquilito <renandgmiquilito@gmail.com>
2024-05-06 21:44:19 -0300

    Add logic for maid cleaning

--- test-03/src/Maid.java
+++ test-03/src/Maid.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class Maid extends Thread {
@@ -17,5 +19,41 @@ public class Maid extends Thread {
 
     public void setOnDuty(Boolean onDuty) {
         this.onDuty = onDuty;
+    }
+
+    private ReentrantLock getKey() {
+        try {
+            if (this.passkey.tryLock(1, TimeUnit.SECONDS)) {
+                try {
+                    if (this.hotel.getKeyStorage().size() > 0) {
+                        int i = new Random().nextInt(this.hotel.getKeyStorage().size());
+                        ReentrantLock key = this.hotel.getKeyStorage().get(i);
+
+                        this.hotel.removeKey(key);
+                        return key;
+                    }
+                } finally {
+                    this.passkey.unlock();
+                }
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    private void clean(ReentrantLock key) {
+        key.lock();
+        try {
+            this.hotel.getRoom(key).getAccess().acquire();
+
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            this.hotel.getRoom(key).getAccess().release();
+            key.unlock();
+        }
     }
 }
