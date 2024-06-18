c31906c45408aec2a70fe5942bf032c42a081c76
Renan Miquilito <renandgmiquilito@gmail.com>
2024-06-17 21:52:09 -0300

    feat: Implement switch statement logic

--- test06/src/main/java/com/example/LibrarianHandler.java
+++ test06/src/main/java/com/example/LibrarianHandler.java
@@ -21,21 +21,54 @@ public class LibrarianHandler extends Thread {
             while ((option = (String) ois.readObject()) != null) {
                 switch (option) {
                     case "listBooks":
+                        Server.library.listBooks(oos);
                         break;
 
                     case "listMembers":
+                        Server.library.listMembers(oos);
                         break;
 
                     case "rent":
+                        oos.writeObject("Name??");
+                        oos.flush();
+                        String name = (String) ois.readObject();
+                        oos.writeObject("Title??");
+                        oos.flush();
+                        String title = (String) ois.readObject();
+                        Boolean response = Server.library.rentBook(title, name);
+                        String message = response ? "Rented." : "Invalid.";
+                        oos.writeObject(message);
+                        oos.flush();
                         break;
 
                     case "add":
+                        String example = "Example: {\"title\": \"Suma Teológica\", \"author\": \"Santo Tomás de Aquino\", \"genre\": \"Teologia\", \"amount\": 1}\n";
+                        oos.writeObject(example + "Object??");
+                        oos.flush();
+                        String object = (String) ois.readObject();
+                        response = Server.library.registerBook(object);
+                        message = response ? "Added." : "Invalid.";
+                        oos.writeObject(message);
+                        oos.flush();
                         break;
 
                     case "return":
+                        oos.writeObject("Member name??");
+                        oos.flush();
+                        name = (String) ois.readObject();
+                        oos.writeObject("Book title??");
+                        oos.flush();
+                        title = (String) ois.readObject();
+                        response = Server.library.returnBook(title, name);
+                        message = response ? "Returned." : "Invalid.";
+                        oos.writeObject(message);
+                        oos.flush();
                         break;
 
                     case "finish":
+                        System.out.println(socket + " finished.");
+                        oos.writeObject(null);
+                        oos.flush();
                         return;
 
                     default:
