54b8c11a319b5715c8d3b54c812135df1e8e0766
Renan Miquilito <renandgmiquilito@gmail.com>
2024-06-17 21:56:24 -0300

    Add auxiliary methods

--- test06/src/main/java/com/example/Library.java
+++ test06/src/main/java/com/example/Library.java
@@ -1,8 +1,11 @@
 package com.example;
 
+import java.io.File;
 import java.io.ObjectOutputStream;
 import java.util.List;
 import java.util.concurrent.locks.ReentrantLock;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Library {
     private List<Book> books;
@@ -14,6 +17,19 @@ public class Library {
 
     public void setBooks(List<Book> books) {
         this.books = books;
+    }
+
+    public void refreshLibrary() throws Exception {
+        new ObjectMapper().writeValue(new File("src/main/resources/books.json"), Server.library);
+    }
+
+    public Book fetchBook(String title) {
+        for (Book book : books) {
+            if (book.getTitle().equals(title)) {
+                return book;
+            }
+        }
+        return null;
     }
 
     public void listBooks(ObjectOutputStream oos) throws Exception {
--- test06/src/main/java/com/example/Members.java
+++ test06/src/main/java/com/example/Members.java
@@ -1,6 +1,10 @@
 package com.example;
 
+import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Members {
     private List<Member> members;
@@ -11,6 +15,35 @@ public class Members {
 
     public void setMembers(List<Member> members) {
         this.members = members;
+    }
+
+    public void refreshMembers() throws Exception {
+        new ObjectMapper().writeValue(new File("src/main/resources/members.json"), Server.members);
+    }
+
+    public Member fetchMember(String name) {
+        for (Member member : members) {
+            if (member.getName().equals(name)) {
+                return member;
+            }
+        }
+        return null;
+    }
+
+    public Book fetchBook(String title, String name) {
+        List<Book> books = new ArrayList<>();
+        for (Member member : members) {
+            if (member.getName().equals(name)) {
+                books = member.getBooks();
+                break;
+            }
+        }
+        for (Book book : books) {
+            if (title.equals(book.getTitle())) {
+                return book;
+            }
+        }
+        return null;
     }
 
     public Member registerMember(String name) {
